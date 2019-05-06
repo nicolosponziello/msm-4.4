57d80a50bc6b7a4eda2f28836bb34b41c1f2c576
Danny Lin <danny@kdrag0n.dev>
2019-05-05 21:59:37 -0700

    cpuidle: lpm-levels: Remove debug event logging
    A measurably significant amount of CPU time is spent on logging events
    for debugging purposes in lpm_cpuidle_enter. Kill the useless logging to
    reduce overhead.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg.c
+++ drivers/power/supply/qcom/qpnp-fg.c
@@ -7996,6 +7996,7 @@ static int fg_common_hw_init(struct fg_chip *chip)
 	int rc;
 	int resume_soc_raw;
 	u8 val;
+	u8 buf[2];
 
 	update_iterm(chip);
 	update_cutoff_voltage(chip);
@@ -8139,6 +8140,23 @@ static int fg_common_hw_init(struct fg_chip *chip)
 		pr_err("failed to write to 0x4B3 rc=%d\n", rc);
 		return rc;
 	}
+
+	/*
+	 * At the end of this function set cut off SOC Ki coefficient from 32 to 96,
+	 * 0x408 offset 0 and 1 to 0xAA00
+	 */
+	buf[0] = 0x00;
+	buf[1] = 0xAA;
+	rc = fg_mem_write(chip, buf, 0x408, 2, 0, 1);
+	if (rc < 0)
+		pr_err("Error in configuring Sram cut off soc thread, rc=%d\n", rc);
+
+	/* Modify cut off current to 200mA. */
+	buf[0] = 0x1F;
+	buf[1] = 0x5;
+	rc = fg_mem_write(chip, buf, 0x410, 2, 0, 1);
+	if (rc < 0)
+		pr_err("Error in configuring Sram cut off current thread, rc=%d\n", rc);
 
 	return 0;
 }
@@ -8597,6 +8615,9 @@ static int fg_detect_pmic_type(struct fg_chip *chip)
 				pmic_rev_id->pmic_subtype);
 		return -EINVAL;
 	}
+
+	if (PMI8996 == pmic_rev_id->pmic_subtype)
+		fg_reset_on_lockup = 1;
 
 	return 0;
 }
